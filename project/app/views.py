8bff828c3e78851513effb7f1f039a6652c04ffd
vaishali <guptavaishali628@gmail.com>
2024-07-06 08:51:39 +0530

    first commit

--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def home1(request):
+    return render(request,'home1.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -21,4 +21,5 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home,name='home'),
+    path('home1/',views.home1, name='home1'),
 ]
